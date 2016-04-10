9b101fb22b502a00735fb80bc1cc6b28158d8e6d
adhe223 <adhe223@g.uky.edu>
2016-04-10 15:17:31 -0500

    Change port

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var path = require('path');
 var $ = require('cheerio');
 var app = express();
+var port = process.env.PORT || 3000;
 
 var gameDict = new Object();
 
@@ -46,6 +47,6 @@ app.get('/', function (req, res) {
 	});
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(port, function () {
+  console.log('Listening on port ' + port + '!');
 });
